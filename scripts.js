d35e6ed42996d4c3e4b7b4d09338b6ecaf3ae1cd
Kubset <kuba1228334@gmail.com>
2018-07-15 15:24:27 +0200

    add localstorage functionality

--- scripts.js
+++ scripts.js
@@ -1,32 +1,49 @@
 
-function newWindow() {
+function loadStorage() {
+    for(let i=0; i<localStorage.length; i++) {
+        newWindow(localStorage.key(i), localStorage.getItem(localStorage.key(i)));    
+    }
+
+}
+
+function saveStorage() {
+    localStorage.clear();
+    var notes = document.getElementsByClassName("note");
+    for(let i=0; i<notes.length; i++) {
+        let content = notes[i].getElementsByTagName("textarea");
+        console.log(content[0].value + "    " + content[1].value);
+        localStorage.setItem(content[0].value, content[1].value);
+    }
+}
+
+
+function newWindow(header, body) {
     var mainDiv = document.getElementById("main-content");
     var injectDiv = document.createElement("div");
     injectDiv.setAttribute("class", "card note");
 
-    //TODO: REFACTOR
-    const cardFiller =      "<div class=\"card-header bg-secondary\">" +
-                                "<textarea class=\"bg-secondary\" row=\"1\" >Press to add note header</textarea>" +
-                            "</div>" +
-                            "<div class=\"card-body bg-light\">" +
-                                "<textarea class=\"bg-light\" row=\"1\">Press to add note body</textarea>" +
-                                "<i class=\"fas fa-trash delete-card\"></i>" +
-                            "</div>" ;
+    const noteFiller = `<div class="card-header bg-secondary">
+                            <textarea class="bg-secondary" row="1" >${header}</textarea>
+                        </div>
+                        <div class="card-body bg-light">
+                            <textarea class="bg-light" row="1">${body}</textarea>
+                            <i class="fas fa-trash delete-card"></i>
+                        </div>`
 
-    injectDiv.innerHTML = cardFiller;
+    injectDiv.innerHTML = noteFiller;
     mainDiv.appendChild(injectDiv);
 
     addListeners();
 }
 
 
-
-
-
 function deleteCard() {
     this.parentNode.parentNode.remove();
-}
 
+    var key =this.parentNode.parentNode.getElementsByTagName("textarea")[0].innerHTML;
+    localStorage.removeItem(key);
+    
+}
 
              
 function autosize(){
@@ -37,24 +54,30 @@ function autosize(){
   },0);
 }
 
+
 function addListeners() {
+    addEventListener("beforeunload", saveStorage)
 
-    var textarea = document.getElementsByTagName("textarea");
-    for(var i=0; i<textarea.length; i++) {
-        textarea[i].addEventListener('keydown', autosize);
+    var textAreas = document.getElementsByTagName("textarea");
+    for(let i=0; i<textAreas.length; i++) {
+        textAreas[i].addEventListener('keydown', autosize);
     }
 
-
-    var cardsBin = document.getElementsByClassName("delete-card");
-
-    for(var j=0; j<cardsBin.length; j++) {
-        cardsBin[j].addEventListener('click', deleteCard);
+    var notesBin = document.getElementsByClassName("delete-card");
+    for(let i=0; i<notesBin.length; i++) {
+        notesBin[i].addEventListener('click', deleteCard);
     }
-
 }
 
 
-window.onload = addListeners();
+window.onload = function() {
+    loadStorage();
+    var textAreas = document.getElementsByTagName("textarea");
+    for(let i=0; i<textAreas.length; i++) {
+        autosize.call(textAreas[i])
+    }
+}
+
 
 
 
